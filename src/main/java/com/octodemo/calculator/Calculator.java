f9c037a4b132877aa89fcadd95dfeb9f2d4f7f17
helaili <helaili@github.com>
2015-06-05 10:45:00 +0200

    Update Calculator.java

--- src/main/java/com/octodemo/calculator/Calculator.java
+++ src/main/java/com/octodemo/calculator/Calculator.java
@@ -3,7 +3,6 @@ package com.github.demo;
 public class Calculator {
 	// This is an addition
 	// That's a badass comment!
-	That's a syntax error
 	public double add(double a, double b) {
 		return a+b;
 	}
