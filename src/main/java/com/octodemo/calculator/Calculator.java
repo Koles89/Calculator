c2aeac65c6573351787462c1cefffec8cd6dcf90
helaili <helaili@github.com>
2015-06-05 10:48:41 +0200

    Update Calculator.java

--- src/main/java/com/octodemo/calculator/Calculator.java
+++ src/main/java/com/octodemo/calculator/Calculator.java
@@ -2,7 +2,7 @@ package com.github.demo;
 
 public class Calculator {
 	// This is an addition
-	// That's a badass comment!
+	// That's a badass comment!!!
 	public double add(double a, double b) {
 		return a+b;
 	}
