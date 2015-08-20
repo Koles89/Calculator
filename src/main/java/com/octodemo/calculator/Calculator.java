e95668be0a60191c35211651948c2601cc69db35
Steve Jobs <octodemo+technical-lead@github.com>
2015-08-20 11:27:56 +0200

    Update Calculator.java

--- src/main/java/com/octodemo/calculator/Calculator.java
+++ src/main/java/com/octodemo/calculator/Calculator.java
@@ -8,16 +8,4 @@ public class Calculator {
 		return a+b;
 	}
 	
-	public double sub(double a, double b) {
-		return a-b;
-	}
-	// Division
-	public double div(double a, double b) {
-		return a/b;
-	}
-	//MUltiplication
-	public double mul(double a, double b) {
-		return a*b;
-	}
-	
 }
