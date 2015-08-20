983588ff0ff86115683c8650e851eef128dda14a
Steve Jobs <octodemo+technical-lead@github.com>
2015-08-20 11:28:17 +0200

    Update CalculatorTest.java

--- src/test/java/com/octodemo/calculator/CalculatorTest.java
+++ src/test/java/com/octodemo/calculator/CalculatorTest.java
@@ -11,11 +11,5 @@ public class CalculatorTest {
 		Calculator calc = new Calculator();
 		assertEquals(4, calc.add(2, 2), 0);
 	}
-	
-	@Test
-	public void testSub() {
-		Calculator calc = new Calculator();
-		assertEquals(4, calc.sub(6, 2), 0);
-	}
 
 }
