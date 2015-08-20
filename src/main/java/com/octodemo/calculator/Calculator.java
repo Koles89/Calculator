1798a23227ccfe63ebda9629ab43a71cdddbc37e
Alain Hélaïli <helaili@github.com>
2015-08-20 11:34:53 +0200

    Implemented Substraction

--- src/main/java/com/octodemo/calculator/Calculator.java
+++ src/main/java/com/octodemo/calculator/Calculator.java
@@ -3,9 +3,13 @@ package com.github.demo;
 public class Calculator {
 	// This is an addition
 	// That's a badass comment!!???
-	
+
 	public double add(double a, double b) {
 		return a+b;
 	}
-	
+
+	public double sub(double a, double b) {
+		return a-b;
+	}
+
 }
