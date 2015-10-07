0ddc19a7824bbe74e38b013152d3ca53c85a706c
helaili <helaili@github.com>
2015-10-07 15:46:11 +0200

    Update CalculatorTest.java

--- src/test/java/com/octodemo/calculator/CalculatorTest.java
+++ src/test/java/com/octodemo/calculator/CalculatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+//TEST2
 	@Test
 	public void testAdd() {
 		Calculator calc = new Calculator();
