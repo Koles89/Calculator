644e24692fb2ef8cc384054b2ec4e0892055092b
helaili <helaili@github.com>
2016-01-07 12:12:20 +0100

    Improving code

--- src/main/java/com/octodemo/calculator/Calculator.java
+++ src/main/java/com/octodemo/calculator/Calculator.java
@@ -6,18 +6,19 @@ public class Calculator {
 		return a+b;
 	}
 	
-	//Un autre commentaire top nvhfgh
+	//Un autre commentaire top
 	public double sub(double a, double b) {
 		return a-b;
 	}
 
 	
 	public double multiply(double a, double b) {
+		//This is some Airbus code
 		return a*b;
 
 	}
 
-	//Commentaire super top gvtvtvtv
+	//Commentaire super top 
 	public double div(double a, double b) {
 		return a/b;
 	}
