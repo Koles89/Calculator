666cb33fdf78895e0ed6060319f92a95098b2966
Alain Hélaïli <helaili@github.com>
2017-04-27 16:19:17 +0200

    New comment in source

--- src/main/java/com/octodemo/calculator/Calculator.java
+++ src/main/java/com/octodemo/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.octodemo.calculator;
 public class Calculator {
 
 	public double add(double a, double b) {
-// Implementing some kickass code pour le switch
+// Implementing some kickass code for Air Liquide
 		return (a+b);
 	}
 
